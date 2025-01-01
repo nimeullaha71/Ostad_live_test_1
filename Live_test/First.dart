abffd38a677c231a66df3795c9467c7d76d8a6be
nimeullaha71 <nimeullahacse@gmail.com>
2025-01-01 23:08:21 +0600

    commit

--- Live_test/First.dart
+++ Live_test/First.dart
@@ -8,7 +8,7 @@ void triangle() {
   print('Enter the height of the triangle: ');
   double height = double.parse(stdin.readLineSync()!);
 
-  double area = 0.5 * base * height;
+  double area = 0.5*base*height;
 
   print('The area of the triangle is: $area');
 }
